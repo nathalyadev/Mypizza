df7b4bd7ad9c7325146dfe2c91bc96d802480879
nathalyadev <ns.contato21@gmail.com>
2022-11-29 16:42:47 -0300

    Adicionando Vue JS

---
+++ main.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+createApp({
+    template: `
+            <nav>
+                <ul>
+                    <li><img src="" alt=""></li>
+                    <li>Início</li>
+                    <li>Menu</li>
+                    <li>Contato</li>
+                    <li>Sobre nós</li>
+                </ul>
+            </nav>
+          <header>
+            <img src="">
+          </header>  
+        `,
+    data() {
+        return {};
+    },
+}).mount("#app");
